c42835e4fe8a8aeaf24308f0a23c772022a071b4
John Cusick <jmorgancusick@gmail.com>
2020-01-20 16:11:59 +1000

    Update update_minecraft_server.py

--- update_minecraft_server.py
+++ update_minecraft_server.py
@@ -7,8 +7,7 @@ import logging
 import sys 
 import os 
  
-version_manifest_url = 'https://launchermeta.mojang.com/mc/game/version_manifest.jso 
-n' 
+version_manifest_url = 'https://launchermeta.mojang.com/mc/game/version_manifest.jso n' 
 jar_directory = '/home/minecraft/minecraft_server_jars' 
 latest_jar_filename = 'minecraft_server_latest.jar' 
  
@@ -33,8 +32,7 @@ def get_server_jar(snapshot_url, jar_file_path):
  
 def update_latest_jar_symlink(jar_file_path): 
     latest_jar_file_path = os.path.join(jar_directory, latest_jar_filename) 
-    # work around to overwrite any existing symlink (see https://stackoverflow.com/a 
-/27788271) 
+    # work around to overwrite any existing symlink (see https://stackoverflow.com/a27788271) 
     os.symlink(jar_file_path, latest_jar_file_path + '.tmp') 
     os.replace(latest_jar_file_path + '.tmp', latest_jar_file_path) 
  
@@ -53,8 +51,7 @@ def main():
             break
  
     if not latest_release:
-        logging.error('Could not find latest release {} in the version manifest'.for
-mat(latest_release_id))
+        logging.error('Could not find latest release {} in the version manifest'.format(latest_release_id))
         return 1
 
     logging.debug('latest release: {}'.format(latest_release['id']))
@@ -64,15 +61,13 @@ mat(latest_release_id))
     jar_file_path = os.path.join(jar_directory, jar_filename)
 
     if os.path.exists(jar_file_path):
-        logging.debug('Already have latest Minecraft server located at {}'.format(ja
-r_file_path))
+        logging.debug('Already have latest Minecraft server located at {}'.format(jar_file_path))
         return 0
 
     get_server_jar(latest_release['url'], jar_file_path)
     update_latest_jar_symlink(jar_file_path)
 
-    logging.debug('successfully updated minecraft server, latest jar located at {}'.
-format(jar_file_path))
+    logging.debug('successfully updated minecraft server, latest jar located at {}'.format(jar_file_path))
 
 if __name__ == '__main__':
     main()
