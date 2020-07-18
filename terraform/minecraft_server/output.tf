c0acfb2ef581e0e1bc44645a686273a89e415e19
John Cusick <losthuntet27@gmail.com>
2020-07-18 10:56:51 -0600

    add assume role arn to output

--- terraform/minecraft_server/output.tf
+++ terraform/minecraft_server/output.tf
@@ -2,3 +2,8 @@ output "IP" {
   value = aws_instance.minecraft.public_ip
   description = "The minecraft server ip"
 }
+
+output "AssumeRoleARN" {
+  value = aws_iam_role.ccg_minecraft_assumed_role.arn
+  description = "The role arn for s3 privileges"
+}
