8fda02236550f5ae849daef38443e033e08b1166
John Cusick <losthuntet27@gmail.com>
2020-07-18 01:51:41 -0600

    IAM minecraft ec2 assume role and s3 priv

--- terraform/minecraft_server/data.tf
+++ terraform/minecraft_server/data.tf
@@ -17,3 +17,49 @@ data "aws_route53_zone" "custom_cloud_gaming" {
   name         = "customcloudgaming.com."
   private_zone = false
 }
+
+data "aws_s3_bucket" "world_bucket" {
+  bucket = var.world_bucket
+}
+
+data "template_file" "ccg_minecraft_sts_assume_policy" {
+  template = file("${path.module}/resources/AllowAssumeRole.tpl")
+
+  vars = {
+    implicit_role_arn = aws_iam_role.ccg_minecraft_implicit_role.arn
+  }
+}
+
+data "template_file" "ccg_minecraft_permit_s3" {
+  template = file("${path.module}/resources/CcgMinecraftIninePolicyS3.tpl")
+
+  vars = {
+    bucket = var.world_bucket
+  }
+}
+
+data "aws_iam_policy_document" "ccg_minecraft_implicit_policy" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "ccg_minecraft_assumed_policy" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.ccg_minecraft_implicit_role.arn]
+    }
+  }
+}
