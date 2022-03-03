c45dc2d9212821fa8656c1492aa66d7edd8a5568
Jack Cusick <jmorgancusick@gmail.com>
2022-03-03 00:30:43 -0700

    use 2GB min, and available for max

--- scripts/start_minecraft_server.sh
+++ scripts/start_minecraft_server.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-RAM=$(free -m | awk '/Mem:/ { print $2 } /buffers\/cache/ { print $3 }')
+MIN_RAM=2000
+AVAIL_RAM=$(free -m | awk '/Mem:/ { print $7 }')
 
 # Update the server jar
 ${DIR}/update_minecraft_server.py 2>&1 | tee /dev/tty | systemd-cat -t minecraft
 
 # Start the server with screen and redirect output to systemd
-/usr/bin/java "-Xms${RAM}M" "-Xmx${RAM}M" -jar /home/minecraft/minecraft_server/server.jar nogui 2>&1 | tee /dev/tty | systemd-cat -t minecraft
+/usr/bin/java "-Xms${MIN_RAM}M" "-Xmx${AVAIL_RAM}M" -jar /home/minecraft/minecraft_server/server.jar nogui 2>&1 | tee /dev/tty | systemd-cat -t minecraft
